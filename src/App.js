6bbe2c25878372fd7439fad0d1941ac3a44605c1
ougrid <ougridd@gmail.com>
2022-12-26 10:26:21 +0700

    Add Home.js | Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
 import React, { useState, useEffect } from "react"
 import axios from "axios"
-import TableView from "./components/TableView"
+import TableView from "./components/TableView/TableView"
+import Home from "./components/Home/Home"
+import { Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <homepage/> */}
-      <h1>Welcome to MONey Spendee</h1>
-      <h2>View Transaction | Add Transaction | Update Personal Info</h2>
-      <p>Is this the homepage, or are we gonna create another one?</p>
-      <TableView/>
+      <nav>
+        <Link to="/">
+          <h2>Home</h2>
+        </Link>
+        <Link to="/table-view">
+          <h2>View Transaction</h2>
+        </Link>
+      </nav>
+      <main>
+        <div>
+          <h1>Welcome to MONey Spendee</h1>
+          {/* <h2>View Transaction | Add Transaction | Update Personal Info</h2> */}
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/table-view" element={<TableView />}/>
+
+        </Routes>
+
+      </main>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h3>This is the homepage!</h3>
+      <p></p>
+    </div>
+  )
+}
+
+export default Home
