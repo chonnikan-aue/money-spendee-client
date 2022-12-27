6d15dac25305f99a60eed2499bb65754a7369a25
ougrid <ougridd@gmail.com>
2022-12-27 11:19:00 +0700

    Import Dropdown and edit "View Transaction"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import React, { useState, useEffect } from "react"
 import axios from "axios"
-import TableView from "./components/TableView/TableView"
 import Home from "./components/Home/Home"
+import TableView from "./components/TableView/TableView"
+import DashboardView from "./components/DashboardView/DashboardView"
+import SummaryView from "./components/SummaryView/SummaryView"
 import AddTransaction from "./components/AddTransaction/AddTransaction"
 import UpdateInfo from "./components/UpdateInfo/UpdateInfo"
 import { Route, Routes, Link, Navigate } from "react-router-dom";
@@ -55,24 +57,14 @@ function App() {
         <Link to="/">
           <Button>Home</Button>
         </Link>
-        {/* BUG: Wrong links */}
-        {/* <Link to="/table-view">  */}
-          <select class="form-select" id="">
-            <option value="">Table View</option>
-            <option value="" onclick = {() => {window.location.href = "/table-view"}}>Table View</option>
-            <option value="" >View Transaction</option>
-            <option value="">Dashboard View</option>
-            <option value="">Summary View</option>
-          </select>
-        {/* </Link> */}
         <Dropdown>
           <Dropdown.Toggle variant="success" id="dropdown-basic">
             View Transaction
           </Dropdown.Toggle>
           <Dropdown.Menu>
             <Dropdown.Item href="/table-view">Table View</Dropdown.Item>
-            <Dropdown.Item href="/dashboard-view">Table View</Dropdown.Item>
-
+            <Dropdown.Item href="/dashboard-view">Dashboard View</Dropdown.Item>
+            <Dropdown.Item href="/summary-view">Summary View</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
         <Link to="/add-transaction">
@@ -86,6 +78,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/table-view" element={<TableView />}/>
+          <Route path="/dashboard-view" element={<DashboardView />}/>
+          <Route path="/summary-view" element={<SummaryView />}/>
           <Route path="/add-transaction" element={<AddTransaction />}/>
           <Route path="/update-info" element={<UpdateInfo />}/>
         </Routes>
