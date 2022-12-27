0b79afaab3981c63b2e85da55b5ef72a72d96e47
ougrid <ougridd@gmail.com>
2022-12-27 12:13:38 +0700

    Import Pagination | Add pages in TableView.js

--- src/components/DashboardView/DashboardView.js
+++ src/components/DashboardView/DashboardView.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react"
 import axios from "axios"
+import { Container } from 'react-bootstrap'
+
 const DashboardView = props => {
   return (
-    <div>
+    <Container>
       <h2>DashboardView Component</h2>
       <p>Pie Chart goes here</p>
-    </div>
+    </Container>
   )
 }
 
--- src/components/TableView/TableView.js
+++ src/components/TableView/TableView.js
@@ -1,38 +1,55 @@
 import React, { useState } from "react"
 import axios from "axios"
-import { Table } from "react-bootstrap"
+import './TableView.css';
+import { Table, Pagination } from "react-bootstrap"
+
 const TableView = props => {
+
+  let active = 1;
+  let items = [];
+    for (let number = 1; number <= 10; number++) {
+      items.push(
+        <Pagination.Item key={number} active={number === active}>
+          {number}
+        </Pagination.Item>,
+      );
+    }
+
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th scope="col">Date</th>
-          <th scope="col">Title</th>
-          <th scope="col">Type</th>
-          <th scope="col">Amount</th>
-        </tr>
-      </thead>
-      <tbody>
-    <tr>
-      <th scope="row">2 Jan 2023</th>
-      <td>Fixed Income</td>
-      <td>Income</td>
-      <td>1,000</td>
-    </tr>
-    <tr>
-      <th scope="row">1 Jan 2023</th>
-      <td>Shopping</td>
-      <td>Daily Expenses</td>
-      <td>500</td>
-    </tr>
-    <tr>
-      <th scope="row">1 Jan 2023</th>
-      <td>Bank Deposit</td>
-      <td>Savings</td>
-      <td>500</td>
-    </tr>
-  </tbody> 
-    </Table>
+    <div>
+      <Table>
+        <thead>
+          <tr>
+            <th scope="col">Date</th>
+            <th scope="col">Title</th>
+            <th scope="col">Type</th>
+            <th scope="col">Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">2 Jan 2023</th>
+            <td>Fixed Income</td>
+            <td>Income</td>
+            <td>1,000</td>
+          </tr>
+          <tr>
+            <th scope="row">1 Jan 2023</th>
+            <td>Shopping</td>
+            <td>Daily Expenses</td>
+            <td>500</td>
+          </tr>
+          <tr>
+            <th scope="row">1 Jan 2023</th>
+            <td>Bank Deposit</td>
+            <td>Savings</td>
+            <td>500</td>
+          </tr>
+        </tbody>
+      </Table>
+      <br />
+      <Pagination>{items}</Pagination>
+    </div>
   )
 }
 
