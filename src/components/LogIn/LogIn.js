3ec94186af0548804b89261d397a40352f739876
ougrid <ougridd@gmail.com>
2022-12-28 22:29:16 +0700

    Add log in form inside LogIn.js

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,11 +1,36 @@
 import React from "react";
+import './LogIn.css';
+import { Button, Form, Container, Row, Col} from 'react-bootstrap'
 
 const LogIn = props => {
   return (
-    <div>
-      <h3>This is the log in page!</h3>
-      <p></p>
-    </div>
+    <Container>
+      <Row>
+        <h3>This is the log in page!</h3>
+      </Row>
+      <Row>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="username" placeholder="Enter Username" />
+            <Form.Text className="text-muted">
+              This will be used as your profile name too.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Stay signed in for 30 days" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Row>
+    </Container>
   )
 }
 
