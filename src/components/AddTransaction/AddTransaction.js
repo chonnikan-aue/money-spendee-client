ae82e9e0ce8f569466e4dcddf54de08ef872f8e2
panupong <panuponglovecal@gmail.com>
2022-12-28 15:52:17 +0700

    "style"

--- src/components/AddTransaction/AddTransaction.js
+++ src/components/AddTransaction/AddTransaction.js
@@ -1,6 +1,7 @@
 import './AddTransaction.css'
 import React, { useState } from "react"
 import axios from "axios"
+import 'bootstrap/dist/css/bootstrap.css';
 
 const type = ["food", "transportation", "rentals", "bill"] // Hard code just for testing. The real one will be brought from the DB
 const account = ["income", "saving"]
@@ -29,64 +30,77 @@ const AddTransaction = () => {
 
     axios.post("http://localhost:3001/withdraw", data)
       .then(res => {
-        console.log(res) 
+        console.log(res)
       })
   }
 
   return (
     <div>
-      <h4 className='input'>Title:</h4>
-      
-      <input
-        type="text"
-        name="name"
-        placeholder="What did you pay?"
-        onChange={handleChange}
-      />
-      <br />
-      <h4 className='input'>Amount:</h4>
-      <input
-        type="text"
-        name="amount"
-        placeholder="THB"
-        onChange={handleChange}
-      />
-      <br />
-      <h4 className='input'>Type:</h4>
-      <select
-        name="typeId"
-        onChange={handleChange} >
-        <option disabled selected>Select Type</option>
-        {
-          type.map((type, index) => (
-            <option value={index}>{type}</option>
-          ))
-        }
-      </select>
-      <br />
-      <h4 className='input'>Account:</h4>
-      <select
-        name="withdrawFromId"
-        onChange={handleChange}>
-        <option disabled selected>Select Account</option>
-        {
-          account.map((account, index) => (
-            <option value={index}>{account}</option>
-          ))
-        }
-      </select>
+      <div className="box">
 
-      <h4 className='input'>Date:</h4>
-      <input
-        type="date"
-        name="date"
-        onChange={handleChange}
-      />
-
-      <form onSubmit={handleSubmit}>
-        <input type="submit" value="Save" />
-      </form>
-
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Title:</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="What did you pay?"
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="amount">Amount:</label>
+            <input
+              type="text"
+              name="amount"
+              placeholder="THB"
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="typeId">Type:</label>
+            <select
+              name="typeId"
+              onChange={handleChange}
+              className="form-control"
+            >
+              <option disabled selected>Select Type</option>
+              {
+                type.map((type, index) => (
+                  <option value={index}>{type}</option>
+                ))
+              }
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="withdrawFromId">Account:</label>
+            <select
+              name="withdrawFromId"
+              onChange={handleChange}
+              className="form-control"
+            >
+              <option disabled selected>Select Account</option>
+              {
+                account.map((account, index) => (
+                  <option value={index}>{account}</option>
+                ))
+              }
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="date">Date:</label>
+            <input
+              type="date"
+              name="date"
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+          <input type="submit" value="Save" className="btn btn-primary" />
+        </form>
+      </div>
     </div>
   )
 }
