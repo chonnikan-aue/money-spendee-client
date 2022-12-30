cbcfe40824e793d18f4aa24bf10c960d2a4641a0
Chonnikan <chonnikan.aue@gmail.com>
2022-12-30 09:52:39 +0700

    create component Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,92 @@
 import React from "react";
+import axios from "axios";
+import "./Home.css";
+import { Button, Form, Row, Col, FloatingLabel } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
-const Home = props => {
+const Home = (props) => {
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    props.setLoginData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (e.nativeEvent.submitter.id === "login") {
+      axios
+        .get("http://localhost:3001/auth", props.loginData)
+        .then((res) => {
+          console.log(res);
+          let token = res.data;
+          localStorage.setItem("jwt", token);
+          props.getUserData();
+          navigate("/table-view");
+        })
+        .catch((err) => {
+          alert("Please try again. Username or Password is incorrect.");
+        });
+    } else if (e.nativeEvent.submitter.id === "signup") {
+      axios.post("http://localhost:3001/auth", props.loginData).then((res) => {
+        if (res.data.name === "SequelizeUniqueConstraintError") {
+          alert("This username is already taken. Please try another.");
+        } else {
+          let token = res.data;
+          localStorage.setItem("jwt", token);
+          props.getUserData();
+          navigate("/table-view");
+        }
+      });
+    }
+  };
+
   return (
-    <div>
-      <h3>This is the homepage!</h3>
-      <p></p>
-    </div>
-  )
-}
+    <Row className="home">
+      <Col>
+        <Row className="title">MONey Spendee</Row>
+        <Row className="sub-title">
+          MONey Spendee helps you track spending and follow your budget.
+        </Row>
+        <br />
+      </Col>
+      <Col>
+        <Form onSubmit={handleSubmit}>
+          <FloatingLabel label="Username" className="mb-3">
+            <Form.Control
+              id="username"
+              type="text"
+              placeholder="Username"
+              onChange={handleChange}
+            />
+          </FloatingLabel>
+          <FloatingLabel label="Password">
+            <Form.Control
+              id="password"
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+            />
+          </FloatingLabel>
+          <Button
+            id="login"
+            className="login-btn"
+            variant="primary"
+            type="submit"
+          >
+            Login
+          </Button>
+          &emsp;or&emsp;
+          <Button id="signup" variant="primary" type="submit">
+            Signup
+          </Button>
+        </Form>
+      </Col>
+    </Row>
+  );
+};
 
-export default Home
+export default Home;
