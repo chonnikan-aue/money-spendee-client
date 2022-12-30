01d9e5c6982fc3d20158663782c3a65e550b2147
Chonnikan <chonnikan.aue@gmail.com>
2022-12-30 09:56:12 +0700

    create Route and get user data

--- src/App.js
+++ src/App.js
@@ -1,147 +1,96 @@
-import './App.css';
-import React, { useState, useEffect } from "react"
-import axios from "axios"
-import Home from "./components/Home/Home"
-import TableView from "./components/TableView/TableView"
-import DashboardView from "./components/DashboardView/DashboardView"
-import SummaryView from "./components/SummaryView/SummaryView"
-import AddTransaction from "./components/AddTransaction/AddTransaction"
-import UpdateInfo from "./components/UpdateInfo/UpdateInfo"
-import SignUp from "./components/SignUp/SignUp"
-import LogIn from "./components/LogIn/LogIn"
-import { Route, Routes, Link } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Container, Row, Col, Dropdown } from 'react-bootstrap'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import TableView from "./components/TableView/TableView";
+import DashboardView from "./components/DashboardView/DashboardView";
+import SummaryView from "./components/SummaryView/SummaryView";
+import AddTransaction from "./components/AddTransaction/AddTransaction";
+import UpdateInfo from "./components/UpdateInfo/UpdateInfo";
+import { Route, Routes, Link, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Container, Row, Col, Dropdown } from "react-bootstrap";
 
 function App() {
-     
-  // TBD: Data structure 
-  const [transactions, setTransactions] = useState({
-    deposits: [],
-    withdraws: []
-  }) 
+  const [loginData, setLoginData] = useState({});
+  const [userData, setUserData] = useState({});
 
-  const getDeposits = () => {
-    axios.get('http://localhost:3004/deposit')
-    .then(res => {
-      setTransactions((prevState) => ({
-        ...prevState, 
-        deposits: res.data
-      }))
-
-      // console.log(res.data)
-
-      // console.log(transactions);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-  }
-  
-  useEffect(() => {
-    getDeposits()
-  }, [])
-  
-  const getWithdraws = () => {
-    axios.get('http://localhost:3004/withdraw')
-    .then(res => {      
-      setTransactions((prevState) => ({
-        ...prevState, 
-        withdraws: res.data
-      }))
-
-      // console.log(res.data)
-
-      // console.log(transactions);
-    })
-    .catch((err) => {
-      console.error(err);
-    });        
-  }
-
-  useEffect(() => {
-    getWithdraws()
-  }, [])
+  const getUserData = () => {
+    let token = localStorage.getItem("jwt");
+    axios
+      .get(`http://localhost:3001/user/username/${loginData.username}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setUserData(res.data);
+      });
+  };
 
   return (
     <div className="App">
       <Container>
-        <Row id="app-header">
-          <div>
-            <h1>Welcome to MONey Spendee</h1>
-          </div>
-        </Row>
-        <Row id="nav-bar">
-          <nav>
-            <Col xs={4} md={2}>
-              <Link to="/">
-                <Button>Home</Button>
-              </Link>
-            </Col>
-            <Col xs={4} md={2}>
-              <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                  View Transaction
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item as={Link} to="/table-view">
-                    Table View
-                  </Dropdown.Item>
-                  <Dropdown.Item as={Link} to="/dashboard-view">
-                    Dashboard View
-                  </Dropdown.Item>
-                  <Dropdown.Item as={Link} to="/summary-view">
-                    Summary View
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Col>
-            <Col xs={4} md={2}>
-              <Link to="/add-transaction">
-                <Button>Add Transaction</Button>
-              </Link>
-            </Col>
-            <Col xs={4} md={2}>
-              <Link to="/update-info">
-                <Button>Update Personal Info</Button>
-              </Link>
-            </Col>
-            <Col xs={4} md={2}>
-              <Link to="/sign-up">
-                <Button>Sign Up</Button>
-              </Link>
-            </Col>
-            <Col xs={4} md={2}>
-              <Link to="/log-in">
-                <Button>Log In</Button>
-              </Link>
-            </Col>
-          </nav>
-        </Row>
-      </Container>
-      <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <Home
+                loginData={loginData}
+                setLoginData={setLoginData}
+                getUserData={getUserData}
+              />
+            }
+          />
           <Route
             path="/table-view"
-            element={<TableView transactions={transactions} />}
+            element={
+              <>
+                <Header userData={userData} />
+                {/* <TableView /> */}
+              </>
+            }
           />
           <Route
             path="/dashboard-view"
-            element={<DashboardView transactions={transactions} />}
+            element={
+              <>
+                <Header userData={userData} />
+                {/* <DashboardView /> */}
+              </>
+            }
           />
           <Route
             path="/summary-view"
-            element={<SummaryView transactions={transactions} />}
+            element={
+              <>
+                <Header userData={userData} />
+                {/* <SummaryView /> */}
+              </>
+            }
           />
-          <Route path="/add-transaction" element={<AddTransaction />} />
-          <Route path="/update-info" element={<UpdateInfo />} />
-          <Route path="/log-in" element={<LogIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
+          <Route
+            path="/add-transaction"
+            element={
+              <>
+                <Header userData={userData} />
+                {/* <AddTransaction /> */}
+              </>
+            }
+          />
+          <Route
+            path="/update-info"
+            element={
+              <>
+                <Header userData={userData} />
+                {/* <UpdateInfo /> */}
+              </>
+            }
+          />
         </Routes>
-      </main>
+      </Container>
     </div>
-  )
+  );
 }
 
 export default App;
