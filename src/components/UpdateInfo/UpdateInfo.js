ea923ce9be816ee0b3ec844fab21d2ac60bf1cfe
Chonnikan <chonnikan.aue@gmail.com>
2022-12-31 11:32:33 +0700

    add Edit Fixed Income section

--- src/components/UpdateInfo/UpdateInfo.js
+++ src/components/UpdateInfo/UpdateInfo.js
@@ -21,8 +21,10 @@ import { InputGroup, FloatingLabel } from "react-bootstrap";
 
 const UpdateInfo = (props) => {
   const username = useRef();
+  const fixedIncome = useRef();
   const newDepositTypeName = useRef();
-  const [newDepositType, setNewDepositType] = useState({});
+  const [newDepositTypeData, setnewDepositTypeData] = useState({});
+  const [fixedIncomeData, setFixedIncomeData] = useState({});
 
   const handleProfileSubmit = (e) => {
     e.preventDefault();
@@ -55,8 +57,43 @@ const UpdateInfo = (props) => {
     }
   };
 
+  const handleFixedIncomeChange = (e) => {
+    setFixedIncomeData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+      userId: props.userData.id,
+    }));
+  };
+
+  const handleFixedIncomeSubmit = (e) => {
+    e.preventDefault();
+    if (fixedIncome.current.value) {
+      let token = localStorage.getItem("jwt");
+      axios
+        .put(
+          `http://localhost:3001/user/${props.userData.id}`,
+          fixedIncomeData,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+        .then((res) => {
+          props.getUserData();
+          fixedIncome.current.value = "";
+        })
+        .then(() => {
+          alert("Fixed income has been changed.");
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } else {
+      alert("Fixed income cannot be empty.");
+    }
+  };
+
   const handleNewDepositTypeChange = (e) => {
-    setNewDepositType((prevState) => ({
+    setnewDepositTypeData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
       userId: props.userData.id,
@@ -70,7 +107,7 @@ const UpdateInfo = (props) => {
       axios
         .post(
           `http://localhost:3001/deposit-type/user/${props.userData.id}`,
-          newDepositType,
+          newDepositTypeData,
           {
             headers: { Authorization: `Bearer ${token}` },
           }
@@ -113,7 +150,7 @@ const UpdateInfo = (props) => {
                     ref={username}
                   />
                 </FloatingLabel>
-                <FloatingLabel label="Password">
+                <FloatingLabel label="Password" className="mb-3">
                   <Form.Control
                     id="password"
                     type="password"
@@ -121,20 +158,31 @@ const UpdateInfo = (props) => {
                     onChange={props.handleProfileChange}
                   />
                 </FloatingLabel>
-                <Button
-                  id="login"
-                  className="login-btn"
-                  variant="primary"
-                  type="submit"
-                >
+                <Button id="login" variant="primary" type="submit">
                   Save Changes
                 </Button>
               </Form>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
-            <Accordion.Header>Add Fixed Income</Accordion.Header>
-            <Accordion.Body></Accordion.Body>
+            <Accordion.Header>Edit Fixed Income</Accordion.Header>
+            <Accordion.Body>
+              <Form onSubmit={handleFixedIncomeSubmit}>
+                <FloatingLabel label="Fixed Income" className="mb-3">
+                  <Form.Control
+                    name="fixedIncome"
+                    type="number"
+                    placeholder="Fixed Income"
+                    onChange={handleFixedIncomeChange}
+                    ref={fixedIncome}
+                    defaultValue={props.userData.fixedIncome}
+                  />
+                </FloatingLabel>
+                <Button variant="primary" type="submit">
+                  Save Changes
+                </Button>
+              </Form>
+            </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2">
             <Accordion.Header>Add Deposit Type</Accordion.Header>
