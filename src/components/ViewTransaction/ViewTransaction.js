7d31bf3da0e4e2af1f73d47c877a6e0dba8408e4
ougrid <ougridd@gmail.com>
2023-01-02 15:34:44 +0700

    Import Accordian and TableView in ViewTransaction

--- src/components/ViewTransaction/ViewTransaction.js
+++ src/components/ViewTransaction/ViewTransaction.js
@@ -1,15 +1,36 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown, Container, Row } from "react-bootstrap";
+import { Container, Row, Accordion } from "react-bootstrap";
+import TableView from "../TableView/TableView";
 
 const ViewTransaction = (props) => {
 
   return (
     <Container>
-      <Row>
-        <h1>Test</h1>
+      <Row className="header">Update Personal Info</Row>
+      <Row className="mb-3">
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Table View</Accordion.Header>
+            <Accordion.Body>
+              <TableView
+                userData={props.userData}
+                
+              />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Dashboard View</Accordion.Header>
+            <Accordion.Body>
+              <TableView
+                userData={props.userData}
+                
+              />
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </Row>
     </Container>
-  );
+  )
 };
 
 export default ViewTransaction;
