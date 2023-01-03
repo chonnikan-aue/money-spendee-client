0c8a9dc37cdffe3d06d32fc6dce874396534416f
ougrid <ougridd@gmail.com>
2023-01-03 19:00:41 +0700

    Remove Pagination from TableView

--- src/components/TableView/TableView.js
+++ src/components/TableView/TableView.js
@@ -179,16 +179,6 @@ const TableView = (props) => {
     }
   }, [props.userData])
 
-  let active = 1
-  let items = []
-  for (let number = 1; number <= 10; number++) {
-    items.push(
-      <Pagination.Item key={number} active={number === active}>
-        {number}
-      </Pagination.Item>
-    )
-  }
-
   return (
     <div id="table-div">
       <table
